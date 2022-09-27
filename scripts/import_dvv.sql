bd916c39e3325a4e6079dc3e9b018a957764b263
Riku Oja <riku.oja@kapsi.fi>
2022-09-27 11:19:26 +0300

    Fix jkr rakennus import sql

--- scripts/import_dvv.sql
+++ scripts/import_dvv.sql
@@ -1,10 +1,10 @@
-insert into jkr.rakennus
+insert into jkr.rakennus (prt, kiinteistotunnus, onko_viemari, geom, kayttoonotto_pvm)
 select 
     rakennustunnus as prt,
-    sijaintikiinteistön_tunnus as kiinteistotunnus,
+    "sijaintikiinteistön tunnus" as kiinteistotunnus,
     viemäri::boolean as onko_viemari,
     ST_GeomFromText('POINT('||itä_koordinaatti||' '||pohjois_koordinaatti||')', 3067) as geom,
-    käytössä_olotilanteen_muutospäivä as kayttoonotto_pvm
+    to_date("käytössä_olotilanteen muutospäivä"::text, 'YYYYMMDD') as kayttoonotto_pvm
 from jkr_dvv.rakennus
 on conflict do nothing
 ;
