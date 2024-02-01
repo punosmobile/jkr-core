3254f5aba40c70e6a42594128ee25cc4ca53b14e
Miikka Kallio <miikka@gispo.fi>
2024-02-01 14:06:02 +0200

    Fixed migration version number.
    Fixed migration version number. Added columns to tyhjennysvali table.

--- db/migrations/V2.25.0__Update_tyhjennysvali.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE jkr.tyhjennysvali
-    ADD COLUMN kertaaviikossa INTEGER;
---
+++ db/migrations/V2.26.0__Update_tyhjennysvali.sql
@@ -0,0 +1,6 @@
+ALTER TABLE jkr.tyhjennysvali
+    ADD COLUMN kertaaviikossa INTEGER;
+    ADD COLUMN kertaaviikossa2 INTEGER;
+    ADD COLUMN Voimassaoloviikotalkaen2 SMALLINT;
+    ADD COLUMN Voimassaoloviikotasti2 SMALLINT;
+    ADD COLUMN tyhjennysvali2 SMALLINT;
