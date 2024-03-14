3718ccc319b841d9ad8b57746b7dc9d8ac1c65b4
Ismo Lahtinen <ismo@gispo.fi>
2024-03-14 12:14:33 +0200

    Add different connection parameters for test environment

--- jkrimporter/conf.py
+++ jkrimporter/conf.py
@@ -2,6 +2,9 @@ import os
 
 from dotenv import dotenv_values
 
+from tests.conftest import TEST_ENV
+
+
 # Path to the .env file in the user's %APPDATA%/jkr directory
 dotenv_path = os.path.join(os.getenv('APPDATA'), 'jkr', '.env')
 
@@ -19,13 +22,10 @@ dbconf = {
     "password": env.get("JKR_PASSWORD", None),
     "dbname": env.get("JKR_DB", None),
 }
-dbtestconf = {
-    "host": env.get("JKR_DB_HOST", None),
-    "port": env.get("JKR_TEST_DB_PORT", None),
-    "username": env.get("JKR_USER", None),
-    "password": env.get("JKR_TEST_PASSWORD", None),
-    "dbname": env.get("JKR_TEST_DB", None),
-}
+if TEST_ENV:
+    dbconf["port"] = env.get("JKR_TEST_DB_PORT", None)
+    dbconf["password"] = env.get("JKR_TEST_PASSWORD", None)
+    dbconf["dbname"] = env.get("JKR_TEST_DB", None)
 
 __all__ = ["dbconf"]
 
