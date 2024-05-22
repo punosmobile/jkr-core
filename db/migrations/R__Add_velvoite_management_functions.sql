203a461cce46c86da6d7327021ea81e10bb87602
Ismo Lahtinen <ismo@gispo.fi>
2024-05-22 15:43:22 +0300

    Move all velvoiteyhteenveto stuff to separate tables, move checking top class of yhteenveto to status check

--- db/migrations/R__Add_velvoite_management_functions.sql
+++ db/migrations/R__Add_velvoite_management_functions.sql
@@ -7,11 +7,13 @@ CREATE OR REPLACE FUNCTION jkr.update_velvoitteet(
 AS $BODY$
 DECLARE
   velvoitemalli RECORD;
-  insert_sql text;
+  insert_velvoite_sql text;
+  yhteenvetomalli RECORD;
+  insert_yhteenveto_sql text;
 BEGIN
   FOR velvoitemalli in select id, saanto, voimassaolo from jkr.velvoitemalli where saanto is not null
   loop
-    insert_sql := '
+    insert_velvoite_sql := '
       insert into jkr.velvoite(kohde_id, velvoitemalli_id)
       select
         k.id,
@@ -22,7 +24,22 @@ BEGIN
         and k.voimassaolo && $2
       ON CONFLICT DO NOTHING
     ';
-    EXECUTE insert_sql USING velvoitemalli.id, velvoitemalli.voimassaolo;
+    EXECUTE insert_velvoite_sql USING velvoitemalli.id, velvoitemalli.voimassaolo;
+  end loop;
+  FOR yhteenvetomalli in select id, saanto, voimassaolo from jkr.velvoiteyhteenvetomalli where saanto is not null
+  loop
+    insert_yhteenveto_sql := '
+      insert into jkr.velvoiteyhteenveto(kohde_id, velvoiteyhteenvetomalli_id)
+      select
+        k.id,
+        $1
+      from jkr.kohde k
+      where
+        exists (select 1 from jkr.'||quote_ident(yhteenvetomalli.saanto)||' kohteet where k.id = kohteet.id)
+        and k.voimassaolo && $2
+      ON CONFLICT DO NOTHING
+    ';
+    EXECUTE insert_yhteenveto_sql USING yhteenvetomalli.id, yhteenvetomalli.voimassaolo;
   end loop;
   RETURN 1;
 END;
@@ -63,6 +80,67 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+CREATE OR REPLACE FUNCTION jkr.velvoiteyhteenveto_status(loppupvm date) RETURNS TABLE(velvoiteyhteenveto_id int, jakso daterange, ok bool) AS
+$$
+DECLARE
+  velvoiteyhteenvetomalli RECORD;
+  select_sql text;
+  jakso_alku date := DATE_TRUNC('quarter', loppupvm) - INTERVAL '6 months';
+  jakso_loppu date := DATE_TRUNC('quarter', loppupvm) + INTERVAL '3 months' - INTERVAL '1 day';
+BEGIN
+  CREATE TEMP TABLE temp_status (
+    velvoiteyhteenveto_id int,
+    jakso daterange,
+    ok bool,
+    kohde_id int,
+    luokitus int
+  ) ON COMMIT DROP;
+  FOR velvoiteyhteenvetomalli IN SELECT id, tayttymissaanto FROM jkr.velvoiteyhteenvetomalli WHERE tayttymissaanto IS NOT null
+  LOOP
+    select_sql := '
+      select
+        v.id velvoiteyhteenveto_id,
+        daterange($1, $2) AS jakso,
+        case when ok.kohde_id is not null then true else false end ok,
+        k.id,
+        vm.luokitus
+      from
+        jkr.kohde k
+        join jkr.velvoiteyhteenveto v
+          on k.id = v.kohde_id
+        join jkr.velvoiteyhteenvetomalli vm
+          on v.velvoiteyhteenvetomalli_id = vm.id
+        left join jkr.'||quote_ident(velvoiteyhteenvetomalli.tayttymissaanto)||'(daterange($1, $2)) ok
+          on v.kohde_id = ok.kohde_id
+      where
+          vm.id = $3
+          and k.voimassaolo && daterange($1, $2)
+          and vm.voimassaolo && daterange($1, $2)
+    ';
+    EXECUTE 'INSERT INTO temp_status ' || select_sql USING jakso_alku, jakso_loppu, velvoiteyhteenvetomalli.id;
+  END LOOP;
+  WITH yhteenvedot_joilla_parempi_luokitus AS (
+    SELECT t1.*
+    FROM temp_status t1
+    WHERE EXISTS (
+      SELECT 1
+      FROM temp_status t2
+      WHERE t2.kohde_id = t1.kohde_id
+        AND t2.ok = true
+        AND t2.luokitus < t1.luokitus
+    )
+  )
+  UPDATE temp_status
+  SET ok = false
+  WHERE (kohde_id, luokitus) IN (
+    SELECT kohde_id, luokitus
+    FROM yhteenvedot_joilla_parempi_luokitus
+  );  
+  RETURN QUERY SELECT temp_status.velvoiteyhteenveto_id, temp_status.jakso, temp_status.ok FROM temp_status;
+END;
+$$ LANGUAGE plpgsql;
+
+
 DROP FUNCTION IF EXISTS jkr.tallenna_velvoite_status;
 create or replace function jkr.tallenna_velvoite_status(date) RETURNS int AS
 $$
@@ -74,6 +152,14 @@ $$
       tallennuspvm = CURRENT_DATE
   ;
   REFRESH MATERIALIZED VIEW jkr.v_velvoitteiden_kohteet;
+  INSERT INTO jkr.velvoiteyhteenveto_status (velvoiteyhteenveto_id, jakso, ok, tallennuspvm)
+  select velvoiteyhteenveto_id, jakso, ok, CURRENT_DATE from jkr.velvoiteyhteenveto_status($1)
+  ON CONFLICT (velvoiteyhteenveto_id, jakso) DO UPDATE
+    SET
+      ok = EXCLUDED.ok,
+      tallennuspvm = CURRENT_DATE
+  ;
+  REFRESH MATERIALIZED VIEW jkr.v_velvoiteyhteenvetojen_kohteet;
   SELECT 1;
 $$ LANGUAGE SQL;
 
---
+++ db/migrations/velvoitteet/R__z_Insert_velvoiteyhteenvetomalli.sql
@@ -0,0 +1,165 @@
+INSERT INTO jkr.velvoiteyhteenvetomalli(id, selite, saanto, tayttymissaanto, alkupvm, kuvaus, luokitus)
+VALUES
+    (
+        1,
+        'Velvoiteyhteenveto vapautettu',
+        'kohde',
+        'kohteet_joilla_vapauttava_paatos_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto ei tarvitse jätteenkuljetusta',
+        1
+    ),
+    (
+        2,
+        'Velvoiteyhteenveto keskeytetty',
+        'kohde',
+        'kohteet_joilla_keskeyttava_paatos_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto ei tarvitse jätteenkuljetusta',
+        1
+    ),
+    (
+        30,
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        'v_ei_erilliskeraysalueet',
+        'kohteet_joilla_seka_ok',
+        '2022-1-1',
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        1
+    ),
+    (
+        31,
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        'v_enint_4_huoneistoa_biojatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_kompostointi_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        1
+    ),
+    (
+        32,
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        'v_enint_4_huoneistoa_biojatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4',
+        '2022-1-1',
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        1
+    ),
+    (
+        33,
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4_muut_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto jätteenkuljetus kunnossa',
+        1
+    ),
+    (
+        34,
+        'Velvoiteyhteenveto ei jätehuoltoa',
+        'kohde',
+        'kohteet_joilla_seka_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto ei jätehuoltoa',
+        3
+    ),
+    (
+        35,
+        'Velvoiteyhteenveto biojäte puuttuu',
+        'v_erilliskeraysalueet',
+        'kohteet_joilla_seka_ok_bio_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto biojäte puuttuu',
+        2
+    ),
+    (
+        36,
+        'Velvoiteyhteenveto kartonki puutteellinen',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4_kartonki_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto kartonki puutteellinen',
+        2
+    ),
+    (
+        37,
+        'Velvoiteyhteenveto metalli puutteellinen',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4_metalli_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto metalli puutteellinen',
+        2
+    ),
+    (
+        38,
+        'Velvoiteyhteenveto lasi puutteellinen',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4_lasi_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto lasi puutteellinen',
+        2
+    ),
+    (
+        39,
+        'Velvoiteyhteenveto muovipakkaus puutteellinen',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_ok_bio_enint_4_muovi_puuttuu',
+        '2022-1-1',
+        'Velvoiteyhteenveto muovipakkaus puutteellinen',
+        2
+    ),
+    (
+        40,
+        'Velvoiteyhteenveto sekajäte väärä tyhjennysväli',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_vaara_tvali_muut_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli sekajäte',
+        1
+    ),
+    (
+        41,
+        'Velvoiteyhteenveto sekajäte väärä tyhjennysväli',
+        'v_enint_4_huoneistoa_biojatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_vaara_tvali_bio_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli sekajäte',
+        1
+    ),
+    (
+        42,
+        'Velvoiteyhteenveto sekajäte väärä tyhjennysväli',
+        'v_enint_4_huoneistoa_biojatteen_erilliskeraysalue',
+        'kohteet_joilla_seka_vaara_tvali_kompostointi_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli sekajäte',
+        1
+    ),
+    (
+        43,
+        'Velvoiteyhteenveto sekajäte väärä tyhjennysväli',
+        'v_ei_erilliskeraysalueet',
+        'kohteet_joilla_seka_vaara_tvali',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli sekajäte',
+        1
+    ),
+    (
+        44,
+        'Velvoiteyhteenveto biojäte väärä tyhjennysväli',
+        'v_enint_4_huoneistoa_biojatteen_erilliskeraysalue',
+        'kohteet_joilla_bio_vaara_tvali_seka_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli biojäte',
+        1
+    ),
+    (
+        45,
+        'Velvoiteyhteenveto biojäte väärä tyhjennysväli',
+        'v_vah_5_huoneistoa_hyotyjatteen_erilliskeraysalue',
+        'kohteet_joilla_bio_vaara_tvali_muut_voimassa',
+        '2022-1-1',
+        'Velvoiteyhteenveto väärä tyhjennysväli biojäte',
+        1
+    )
+ON CONFLICT DO NOTHING;
