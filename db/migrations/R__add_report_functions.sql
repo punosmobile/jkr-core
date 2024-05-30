5d38bbd3931774b6e0fc6c5bc24dc454ab4accdd
Miikka Kallio <miikka@gispo.fi>
2024-05-30 18:31:02 +0300

    Changed kohteiden_paatokset to use daterange instead of date.

--- db/migrations/R__add_report_functions.sql
+++ db/migrations/R__add_report_functions.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION jkr.kohteiden_paatokset(kohde_ids integer[], tarkistuspvm date)
+CREATE OR REPLACE FUNCTION jkr.kohteiden_paatokset(kohde_ids integer[], tarkistusjakso daterange)
 RETURNS TABLE(
     Kohde_id integer,
     Kompostoi date,
@@ -27,42 +27,42 @@ BEGIN
     paatokset AS (
         SELECT
             r.kohde_id,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS perusmaksupaatos_voimassa,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS perusmaksupaatos,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS tyhjennysvalipaatos_voimassa,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS tyhjennysvalipaatos,
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS perusmaksupaatos_voimassa,
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS perusmaksupaatos,
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS tyhjennysvalipaatos_voimassa,
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS tyhjennysvalipaatos,
             CASE WHEN 
-                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
-                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END)
+                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
+                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END)
                 ELSE NULL
             END AS akp_kohtuullistaminen_voimassa,
             CASE WHEN 
-                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
-                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END)
+                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
+                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END)
                 ELSE NULL
             END AS akp_kohtuullistaminen,
             CASE WHEN 
-                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
-                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END)
+                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
+                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END)
                 ELSE NULL
             END AS keskeytys_voimassa,
             CASE WHEN 
-                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
-                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END)
+                COUNT(*) = SUM(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN 1 ELSE 0 END) THEN
+                    MAX(CASE WHEN vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END)
                 ELSE NULL
             END AS keskeytys,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS erilliskerayksesta_poikkeaminen_voimassa,
-            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS erilliskerayksesta_poikkeaminen
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.loppupvm END) AS erilliskerayksesta_poikkeaminen_voimassa,
+            MAX(CASE WHEN vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1' THEN vp.paatosnumero END) AS erilliskerayksesta_poikkeaminen
         FROM
             rakennukset r
         LEFT JOIN
             jkr.viranomaispaatokset AS vp ON vp.rakennus_id = r.rakennus_id
         WHERE
-            (vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1')
-            OR (vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1')
-            OR (vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1')
-            OR (vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1')
-            OR (vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo @> tarkistuspaivamaara AND vp.paatostulos_koodi = '1')
+            (vp.tapahtumalaji_koodi = 'PERUSMAKSU' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1')
+            OR (vp.tapahtumalaji_koodi = 'TYHJENNYSVALI' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1')
+            OR (vp.tapahtumalaji_koodi = 'AKP' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1')
+            OR (vp.tapahtumalaji_koodi = 'KESKEYTTAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1')
+            OR (vp.tapahtumalaji_koodi = 'ERILLISKERAYKSESTA_POIKKEAMINEN' AND vp.voimassaolo && tarkistusjakso AND vp.paatostulos_koodi = '1')
         GROUP BY
             r.kohde_id
     ),
@@ -77,7 +77,7 @@ BEGIN
         LEFT JOIN
             jkr.kompostori AS kom ON kom.id = k.kompostori_id
         WHERE
-            kom.voimassaolo @> tarkistuspaivamaara
+            kom.voimassaolo && tarkistusjakso
         ORDER BY
             k_id.kohde_id, kom.loppupvm DESC
     )
@@ -99,7 +99,9 @@ BEGIN
     LEFT JOIN
         composting_status cs ON cs.kohde_id = k_id.kohde_id
     LEFT JOIN
-        paatokset p ON p.kohde_id = k_id.kohde_id;
+        paatokset p ON p.kohde_id = k_id.kohde_id
+    ORDER BY
+        k_id.kohde_id;
 END;
 $$ LANGUAGE plpgsql;
 
