b40fa5503022b67a59eb135d0cc117d2b38c7386
tero-punos <tero.andelin@punosmobile.com>
2024-11-28 07:32:21 +0200

    FIX: Scripts

--- scripts/2024.sh
+++ scripts/2024.sh
@@ -192,5 +192,5 @@ log_exec "jkr import --luo_uudet ../data/Kuljetustiedot/Kuljetustiedot_2024/$qua
         "Q4 kuljetustietojen tuonti"
 
 log_exec "psql -h $HOST -p $PORT -d $DB_NAME -U $USER -c \"select jkr.tallenna_velvoite_status('2024-12-31');\"" \
-        "logs/tietovirrat/2022_$quarter/velvoitteet.log" \
+        "logs/tietovirrat/2024_$quarter/velvoitteet.log" \
         "Q4 velvoitteiden tallennus"
