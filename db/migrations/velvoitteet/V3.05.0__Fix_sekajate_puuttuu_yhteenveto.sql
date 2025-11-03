0c6b9a50bca8ef202506a38c09dcdf444d000fac
tero-punos <tero.andelin@partners.neste.com>
2025-11-03 13:55:32 +0200

    FIX: velvoitetarkastelut

--- db/migrations/velvoitteet/V3.05.0__Fix_sekajate_puuttuu_yhteenveto.sql
+++ db/migrations/velvoitteet/V3.05.0__Fix_sekajate_puuttuu_yhteenveto.sql
@@ -1,16 +1,18 @@
+-- V3.05.0__Fix_sekajate_puuttuu_yhteenveto.sql
+-- LAH-383: Korjaa "sekajäte puuttuu" yhteenveto biohapa-kohteille
+--
+-- Ongelma: Yhteenvetomalli 34 käyttää saanto='kohde' mikä on TAULU, ei näkymä.
+-- update_velvoitteet() funktio vaatii että saanto on näkymä.
+--
+-- Ratkaisu: Luodaan näkymä v_kohde joka palauttaa kaikki kohteet.
+-- Varsinainen suodatus (keillä sekajäte puuttuu) tapahtuu tayttymissaanto-funktiossa
+-- joka ottaa daterange parametrin, joten historiallisten yhteenvetojen luominen toimii.
 
--- Luodaan näkymä joka käyttää kohteet_joilla_seka_puuttuu() funktiota
--- Käytetään viimeisintä kvartaalia (nykyhetki) parametrina
-CREATE OR REPLACE VIEW jkr.v_kohteet_joilla_seka_puuttuu AS
-SELECT kohde_id AS id
-FROM jkr.kohteet_joilla_seka_puuttuu(
-    daterange(
-        (DATE_TRUNC('quarter', CURRENT_DATE) - INTERVAL '6 months')::date,
-        (DATE_TRUNC('quarter', CURRENT_DATE) + INTERVAL '3 months' - INTERVAL '1 day')::date
-    )
-);
+-- Luodaan näkymä joka palauttaa kaikki kohteet
+CREATE OR REPLACE VIEW jkr.v_kohde AS
+SELECT * FROM jkr.kohde;
 
--- Päivitetään yhteenvetomalli 34 käyttämään uutta näkymää
+-- Päivitetään yhteenvetomalli 34 käyttämään näkymää taulun sijaan
 UPDATE jkr.velvoiteyhteenvetomalli
-SET saanto = 'v_kohteet_joilla_seka_puuttuu'
+SET saanto = 'v_kohde'
 WHERE id = 34;
